169266db9aec98469ef621d5a768c0782931f4f9
Amelia Lim <limchornguan418@gmail.com>
2019-02-27 18:11:56 +0100

    like button done, next is to pass likes data to customize page

--- client/src/components/pages/Showcase.jsx
+++ client/src/components/pages/Showcase.jsx
@@ -3,20 +3,41 @@ import api from '../../api';
 import ShowcaseBox from '../ShowcaseBox';
 
 const Showcase = props => {
+  /*********************************
+   * States
+   *********************************/
   const [parallaxData, setParallaxData] = useState(null);
-  const [category, setCategory] = useState('');
-  const [property, setProperty] = useState('');
+  const [likes, setLikes] = useState([]);
+  // const [category, setCategory] = useState('');
+  // const [property, setProperty] = useState('');
 
+  /*********************************
+   * Effect
+   *********************************/
   useEffect(() => {
     api.getAllParallaxData().then(res => {
       setParallaxData(res);
     });
   }, []);
 
+  /*********************************
+   * Event Handler
+   *********************************/
+  const clickLikeHander = id => {
+    const likesTemp = [...likes];
+    likesTemp.includes(id)
+      ? likesTemp.splice(likesTemp.indexOf(id), 1)
+      : likesTemp.push(id);
+    setLikes(likesTemp);
+  };
+
+  /*********************************
+   * Render
+   *********************************/
   if (!parallaxData) {
     return (
       <div className="Showcase">
-        <div className="showcase__scroll-container">
+        <div className="showcase__top-container">
           <h2>Loading...</h2>
         </div>
       </div>
@@ -24,12 +45,19 @@ const Showcase = props => {
   }
   return (
     <div className="Showcase">
-      <div className="showcase__scroll-container">
+      <div className="showcase__top-container">
         <h2>Scroll Down</h2>
       </div>
       <div>
         {parallaxData.map(data => {
-          return <ShowcaseBox key={data._id} data={data} />;
+          return (
+            <ShowcaseBox
+              key={data._id}
+              data={data}
+              onLikeClick={clickLikeHander}
+              likes={likes}
+            />
+          );
         })}
       </div>
     </div>
