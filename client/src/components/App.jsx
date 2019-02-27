875f8eb3087480f669cf454b5c5ee3b74598244a
Amelia Lim <limchornguan418@gmail.com>
2019-02-27 16:28:39 +0100

    showcase version 1 done

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Navbar from "./Navbar";
-import Home from "./pages/Home";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './pages/Home';
 // import AboutUs from "./pages/AboutUs";
-import Showcase from "./pages/Showcase";
+import Showcase from './pages/Showcase';
 // import Customize from "./pages/Customize";
 // import Blog from "./pages/Blog";
-import Secret from "./pages/Secret";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Secret from './pages/Secret';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
--- client/src/components/pages/Showcase.jsx
+++ client/src/components/pages/Showcase.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import ShowcaseTemplate from "../ShowcaseTemplate";
+import React, { useState, useEffect } from 'react';
+import api from '../../api';
+import ShowcaseBox from '../ShowcaseBox';
 
-export default function Showcase() {
+const Showcase = props => {
+  const [parallaxData, setParallaxData] = useState(null);
+  const [category, setCategory] = useState('');
+  const [property, setProperty] = useState('');
+
+  useEffect(() => {
+    api.getAllParallaxData().then(res => {
+      setParallaxData(res);
+    });
+  }, []);
+
+  if (!parallaxData) {
+    return (
+      <div className="Showcase">
+        <div className="showcase__scroll-container">
+          <h2>Loading...</h2>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="Showcase">
-      <ShowcaseTemplate />
+      <div className="showcase__scroll-container">
+        <h2>Scroll Down</h2>
+      </div>
+      <div>
+        {parallaxData.map(data => {
+          return <ShowcaseBox key={data._id} data={data} />;
+        })}
+      </div>
     </div>
   );
-}
+};
+
+export default Showcase;
