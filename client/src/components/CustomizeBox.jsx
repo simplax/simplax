ab0801cb97614b97fa1b5e37c0afa8adae8bfa78
Malte <m.felmy@googlemail.com>
2019-03-01 18:57:31 +0100

    set container/container-fluid and sticky-top-md

--- client/src/components/CustomizeBox.jsx
+++ client/src/components/CustomizeBox.jsx
@@ -4,57 +4,58 @@ import Plx from "react-plx";
 export default function CustomizeBox({ parallaxDataSelection }) {
   const parallaxData = [];
 
-  parallaxDataSelection.forEach(data => {
-    const {
-      _id,
-      start,
-      startOffsetIn,
-      startOffsetOut,
-      end,
-      endOffsetIn,
-      endOffsetOut,
-      easing,
-      startValue,
-      endValue,
-      category,
-      property,
-      unit
-    } = data;
+  if (parallaxDataSelection) {
+    parallaxDataSelection.forEach(data => {
+      const {
+        _id,
+        start,
+        startOffsetIn,
+        startOffsetOut,
+        end,
+        endOffsetIn,
+        endOffsetOut,
+        easing,
+        startValue,
+        endValue,
+        category,
+        property,
+        unit
+      } = data;
 
-    parallaxData.push(
-      {
-        start,
-        startOffset: startOffsetIn,
-        end,
-        endOffset: endOffsetIn,
-        easing,
-        properties: [
-          {
-            startValue,
-            endValue,
-            property,
-            unit
-          }
-        ]
-      },
-      {
-        start,
-        startOffset: startOffsetOut,
-        end,
-        endOffset: endOffsetOut,
-        easing,
-        properties: [
-          {
-            startValue: endValue,
-            endValue: startValue,
-            property,
-            unit
-          }
-        ]
-      }
-    );
-  });
-  console.log("TCL: CustomizeBox -> parallaxData", parallaxData);
+      parallaxData.push(
+        {
+          start,
+          startOffset: startOffsetIn,
+          end,
+          endOffset: endOffsetIn,
+          easing,
+          properties: [
+            {
+              startValue,
+              endValue,
+              property,
+              unit
+            }
+          ]
+        },
+        {
+          start,
+          startOffset: startOffsetOut,
+          end,
+          endOffset: endOffsetOut,
+          easing,
+          properties: [
+            {
+              startValue: endValue,
+              endValue: startValue,
+              property,
+              unit
+            }
+          ]
+        }
+      );
+    });
+  }
 
   /*********************************
    * render
@@ -66,7 +67,7 @@ export default function CustomizeBox({ parallaxDataSelection }) {
           <Plx parallaxData={parallaxData}>
             <div className="showcase-box" />
           </Plx>
-          <div className='scrolldown-container'></div>
+          <div className="scrolldown-container" />
         </div>
       </div>
     </div>
--- client/src/components/pages/Blog.jsx
+++ client/src/components/pages/Blog.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import api from '../../api';
+import React from "react";
+import api from "../../api";
 import { Link } from "react-router-dom";
-
-
 
 export default class Blog extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { blogs: [], abc: "<h1>hi Min <h1>" }
-    this.deleteCountry = this.deleteCountry.bind(this)
+    super(props);
+    this.state = { blogs: [], abc: "<h1>hi Min <h1>" };
+    this.deleteCountry = this.deleteCountry.bind(this);
   }
 
   componentDidMount() {
-    api.getBlogPost()
-      .then(data => this.setState({ blogs: data }))
+    api.getBlogPost().then(data => this.setState({ blogs: data }));
   }
 
   deleteCountry(blogId) {
-    api.deleteBlogPost(blogId)
-      .then(() => this.setState({ blogs: this.state.blogs.filter(blog => blog._id !== blogId) }))
+    api
+      .deleteBlogPost(blogId)
+      .then(() => this.setState({ blogs: this.state.blogs.filter(blog => blog._id !== blogId) }));
   }
 
   render() {
-
     return (
-      <div>
-        {this.state.blogs.map((blog) => (
-          <div key={blog._id} style={{ border: '1px solid black' }}>
-            {blog.title}
-            <div dangerouslySetInnerHTML={{ __html: blog.blogContent }} />
-            <Link to={`/edit-blog/${blog._id}`}>Edit</Link>
-            <button onClick={e => this.deleteCountry(blog._id)}>Delete</button>
-          </div>
-        ))}
+      <div className="Blog">
+        <div className="container">
+          {this.state.blogs.map(blog => (
+            <div key={blog._id} style={{ border: "1px solid black" }}>
+              {blog.title}
+              <div dangerouslySetInnerHTML={{ __html: blog.blogContent }} />
+              <Link to={`/edit-blog/${blog._id}`}>Edit</Link>
+              <button onClick={e => this.deleteCountry(blog._id)}>Delete</button>
+            </div>
+          ))}
+        </div>
       </div>
-    )
+    );
   }
 }
