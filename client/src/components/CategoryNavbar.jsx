7d144d13aa8563cd6783345852393b4275146e3e
Amelia Lim <limchornguan418@gmail.com>
2019-03-01 18:31:41 +0100

    sticky navbar done

--- client/src/components/CategoryNavbar.jsx
+++ client/src/components/CategoryNavbar.jsx
@@ -2,10 +2,42 @@ import React, { useState } from 'react';
 import Plx from 'react-plx';
 import TransitionEnter from './animations/TextTranslateY';
 
-const CategoryNavbar = () => {
+const CategoryNavbar = ref => {
   return (
-    <div className="scroll-down-container">
-      <div className="CategoryNavbar" />
+    <div className="CategoryNavbar">
+      <nav className="navbar navbar-dark navbar-expand-lg">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Transform
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Colors
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                CSS Filter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
