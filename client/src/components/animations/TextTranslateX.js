d8ad19b47948f677599381fcb174ee0162c80780
Amelia Lim <limchornguan418@gmail.com>
2019-03-02 20:02:13 +0100

    pull pull

--- client/src/components/animations/TextTranslateX.js
+++ client/src/components/animations/TextTranslateX.js
@@ -21,7 +21,7 @@ const TextTranslateX = ({ text, isEnter }) => {
     <div className="TextTranslateX">
       <div>
         {trail.map(({ x, opacity }, index) => (
-          <animated.h4
+          <animated.h5
             key={index}
             className="TextTranslateX__text"
             style={{
@@ -30,7 +30,7 @@ const TextTranslateX = ({ text, isEnter }) => {
             }}
           >
             {textArr[index]}
-          </animated.h4>
+          </animated.h5>
         ))}
       </div>
     </div>
