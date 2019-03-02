bfe94890f8d9167c23eba5f229924b3325a1fcfe
Amelia Lim <limchornguan418@gmail.com>
2019-03-02 17:18:28 +0100

    I just want to pull sth

--- client/src/components/animations/PropertyNavbarAnimation.js
+++ client/src/components/animations/PropertyNavbarAnimation.js
@@ -1,40 +1,48 @@
-// text fade in from left to right, fade out from right to left
-// Takes two props:
-// text: string, text that you want to animate
-// isEnter: boolean, determine text enter or exit
+// Props: properties, array of property names
 
 import React, { useState } from 'react';
-import { useTrail, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
-const TextTranslateX = ({ text, isEnter }) => {
-  const textArr = text.split('');
-
-  const config = { mass: 1, tension: 2000, friction: 100 };
-  const trail = useTrail(textArr.length, {
-    config,
-    opacity: isEnter ? 1 : 0,
-    x: isEnter ? 0 : -20,
-    from: { opacity: 0, x: -20 }
+const PropertyNavbarAnimation = ({ properties }) => {
+  const [propertyArr, setProperties] = useState([...properties]);
+  const transitions = useTransition(propertyArr, property => null, {
+    from: { transform: 'translate(0,-40px)' },
+    enter: { transform: 'translate(0,0px)' },
+    leave: { transform: 'translate(0,-40px)' }
   });
 
-  return (
-    <div className="TextTranslateX">
-      <div>
-        {trail.map(({ x, opacity }, index) => (
-          <animated.h4
-            key={index}
-            className="TextTranslateX__text"
-            style={{
-              opacity,
-              transform: x.interpolate(x => `translateX(${x}px)`)
-            }}
-          >
-            {textArr[index]}
-          </animated.h4>
-        ))}
-      </div>
-    </div>
-  );
+  return transitions.map(({ item, props, key }) => (
+    <animated.div key={key} style={props}>
+      {item}
+    </animated.div>
+  ));
+
+  // const config = { mass: 1, tension: 2000, friction: 100 };
+  // const trail = useTrail(properties.length, {
+  //   config,
+  //   opacity: 1,
+  //   x: 0,
+  //   y: 0,
+  //   scale: 1,
+  //   from: { opacity: 0, x: -20, y: -20, scale: 1.5 }
+  // });
+
+  // return (
+  //   <ul className="navbar-nav">
+  //     {trail.map(({ opacity, x, y, scale }, index) => (
+  //       <animated.li
+  //         key={index}
+  //         className="TextTranslateX__text"
+  //         style={{
+  //           opacity,
+  //           transform: x.interpolate(x => `translateX(${x}px)`)
+  //         }}
+  //       >
+  //         <a>hello</a>
+  //       </animated.li>
+  //     ))}
+  //   </ul>
+  // );
 };
 
 export default TextTranslateX;
