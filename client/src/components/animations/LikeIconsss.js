b8c68210eb96f5c2bbab097f8c968b42898b0181
Amelia Lim <limchornguan418@gmail.com>
2019-03-05 09:30:27 +0100

    like button and color theme done

---
+++ client/src/components/animations/LikeIconsss.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LikeIconsss = () => {
+  return (
+    <div className="LikeIconsss">
+      <i id="like-1" className="fas fa-heart fa-1x"></i>
+      <i id="like-2" className="fas fa-heart fa-1x"></i>
+      <i id="like-3" className="fas fa-heart fa-2x"></i>
+      <i id="like-4" className="fas fa-heart fa-2x"></i>
+      <i id="like-5" className="fas fa-heart fa-1x"></i>
+      <i id="like-6" className="fas fa-heart fa-2x"></i>
+      <i id="like-7" className="fas fa-heart fa-2x"></i>
+      <i id="like-8" className="fas fa-heart fa-1x"></i>
+      <i id="like-9" className="fas fa-heart fa-2x"></i>
+      <i id="like-10" className="fas fa-heart fa-1x"></i>
+      <i id="like-11" className="fas fa-heart fa-2x"></i>
+      <i id="like-12" className="fas fa-heart fa-1x"></i>
+    </div>
+  )
+}
+
+export default LikeIconsss
