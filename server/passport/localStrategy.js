e89633384800a68e38c9e6a6d728e39136946ae3
Amelia Lim <limchornguan418@gmail.com>
2019-03-07 10:16:26 +0100

    pull

--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -5,33 +5,32 @@ const bcrypt = require('bcrypt');
 
 var GitHubStrategy = require('passport-github').Strategy;
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.BACKEND_URI + "/api/github-login/callback"
-},
-  (accessToken, refreshToken, profile, cb) => {
-    User.findOne({ _github: profile.id })
-      .then(user => {
-        if (user) {
-          return cb(null, user);
-        }
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.BACKEND_URI + '/api/github-login/callback'
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      User.findOne({ _github: profile.id })
+        .then(user => {
+          if (user) {
+            return cb(null, user);
+          }
 
-        const newUser = new User({
-          _github: profile.id,
-          githubUsername: profile.username,
-          imageUrl: profile.photos[0].value
+          const newUser = new User({
+            _github: profile.id,
+            githubUsername: profile.username,
+            imageUrl: profile.photos[0].value || '/images/logo.svg'
+          });
 
-        });
-
-        newUser.save()
-          .then(user => {
-            console.log("user", user)
+          newUser.save().then(user => {
+            console.log('user', user);
             cb(null, newUser);
-          })
-      })
-      .catch(error => {
-
-      })
-  }
-));
+          });
+        })
+        .catch(error => {});
+    }
+  )
+);
